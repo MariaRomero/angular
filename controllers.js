c1c4374fa0c78bccd0295080f6faf5c973d9ced9
MariaRomero <fantasysystems@gmail.com>
2016-05-31 16:34:51 +0100

    adding Todos

--- controllers.js
+++ controllers.js
@@ -1,3 +1,15 @@
 toDoApp.controller('ToDoController', function() {
-  this.todos = [{ task: "ToDo1", completed: true }, { task: "ToDo2", completed: false }];
+
+  var self = this;
+
+  self.todos = [{ task: "ToDo1", completed: true }, { task: "ToDo2", completed: false }];
+
+  self.addToDo = function(todoText) {
+    self.todos.push({ task: todoText, completed: false });
+  };
+
+  self.removeToDo = function() {
+    self.todos.pop();
+  };
+
 });
